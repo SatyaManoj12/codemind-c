1a57e65c07f7b69995f1ee16d8d316669e7b4cc6
SatyaManoj12 <119724289+SatyaManoj12@users.noreply.github.com>
2022-12-28 22:17:06 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int a;         scanf("%d",&a);         if(a>30)         printf("YES ");         else         printf("NO ");     } }

---
+++ Subscription.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a;
+        scanf("%d",&a);
+        if(a>30)
+        printf("YES
+");
+        else
+        printf("NO
+");
+    }
+}
