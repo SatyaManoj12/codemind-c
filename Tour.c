edf7c17ae52ab857944df693822aa8463de22f2d
SatyaManoj12 <119724289+SatyaManoj12@users.noreply.github.com>
2022-12-28 22:36:37 +0530

    #include<stdio.h> int main() {   int i,n;   scanf("%d",&n);   for(i=1;i<=n;i++)   {        int n,m,c;    scanf("%d%d",&n,&m);    c=(5*n)+(7*m);    printf("%d ",c);   } }

---
+++ Tour.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int i,n;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+       int n,m,c;
+   scanf("%d%d",&n,&m);
+   c=(5*n)+(7*m);
+   printf("%d
+",c);
+  }
+}
