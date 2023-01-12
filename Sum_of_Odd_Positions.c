faf3f1972f9b60bdce562c17590a7371e0557d56
SatyaManoj12 <119724289+SatyaManoj12@users.noreply.github.com>
2023-01-12 19:32:27 +0530

    #include<stdio.h> int main() {     int i,n,sum=0;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)      {         sum=sum+x[i];     }     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0;
+    scanf("%d",&n);
+    int x[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+     {
+        sum=sum+x[i];
+    }
+    }
+    printf("%d",sum);
+}
