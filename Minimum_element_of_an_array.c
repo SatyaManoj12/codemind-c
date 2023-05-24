6b44be81f3223e342938645b74a7df6543643560
SatyaManoj12 <119724289+SatyaManoj12@users.noreply.github.com>
2023-05-24 12:33:51 +0530

    #include<stdio.h> int main() { 	int n,i,max=0; 	scanf("%d",&n); 	int a[n]; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	max=a[0]; 	for(i=1;i<n;i++) 	{ 		if(a[i]<max) 		{ 			max=a[i]; 		} 	} 	printf("%d",max); }

--- Minimum_element_of_an_array.c
+++ Minimum_element_of_an_array.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    int min=a[0];
-    for(i=0;i<n;i++)
-    {
-        if(min>a[i])
-        {
-            min=a[i];
-        }
-    }
-    printf("%d",min);
+	int n,i,max=0;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	max=a[0];
+	for(i=1;i<n;i++)
+	{
+		if(a[i]<max)
+		{
+			max=a[i];
+		}
+	}
+	printf("%d",max);
 }
